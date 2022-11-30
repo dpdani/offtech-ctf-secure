23de8254a6d9edbdb5db025b6eee8ddd5d20b409
Meschini Marcello <otech2ai@users.isi.deterlab.net>
2022-11-30 07:52:44 -0800

    Fixed capture.sh

--- blue/analyzer/capture.sh
+++ blue/analyzer/capture.sh
@@ -7,6 +7,6 @@ DST_IP="10.1.2.2"
 
 ETH=$(ssh $1.${EXPERIMENT}.offtech "sudo ip route get ${DST_IP}" | head -1 | awk '{print $5}')
 
-ssh $1.${EXPERIMENT}.offtech "sudo rm -f pcap.pcap"
-ssh $1.${EXPERIMENT}.offtech "sudo tcpdump -s0 -U --immediate-mode -nn -i ${ETH} -w pcap.pcap"
+ssh $1.${EXPERIMENT}.offtech "sudo rm -f ~/offtech-ctf-secure/blue/analyzer/pcap.pcap"
+ssh $1.${EXPERIMENT}.offtech "sudo tcpdump -s0 -U --immediate-mode -nn -i ${ETH} -w  ~/offtech-ctf-secure/blue/analyzer/pcap.pcap"
 
