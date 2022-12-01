d91906a4233d135713646003bdf5459d39fac844
alan-gassine <gassinealan@gmail.com>
2022-12-01 07:06:56 -0500

    Fixed deposit & withdrawal

--- Students_Deterlab_NS/process.php
+++ Students_Deterlab_NS/process.php
@@ -13,14 +13,19 @@ $choice = clear_input($_GET["drop"]);
 
 if (is_null($user) || is_null($pass) || is_null($choice)) {
   print "Error: all the fields must be filled";
+  print "Back to <A HREF='index.php'>home</A>";	
 } else if (!ctype_alnum($user)) {
   print "Error: username can only contain alphanumeric characters.";
+  print "Back to <A HREF='index.php'>home</A>";	
 } else if (strlen($user) > 20) {
   print "Error: username must contain less than 20 characters."; // Increase the size in the db?
+  print "Back to <A HREF='index.php'>home</A>";	
 } else if (strlen($pass) < 8 || strlen($pass) > 50) {
   print "Error: password must be contain 8 to 50 characters.";
+  print "Back to <A HREF='index.php'>home</A>";	
 } else if (!is_numeric($amount) || ($amount <= 0 && ($choice == "deposit" || $choice == "withdraw"))) {
   print "Error: amount must be above 0.";
+  print "Back to <A HREF='index.php'>home</A>";	
 } 
 else {
   $mysqli = new mysqli('localhost', 'root', 'rootmysql', 'ctf2');
@@ -39,6 +44,7 @@ else {
   {
     if(!authenticate($user, $pass)) {
       print "Error: username and/or password is incorrect.";
+      print "Back to <A HREF='index.php'>home</A>";	
     } else {
       $stmt = $mysqli->prepare("select * from transfers where user=?");
       $stmt->bind_param("s", $user);
@@ -69,13 +75,12 @@ else {
   {
     if(!authenticate($user, $pass)) {
       print "Error: username and/or password is incorrect.";
+      print "Back to <A HREF='index.php'>home</A>";	
     } else {
-      // $stmt = $mysqli->prepare("insert into transfers (user,amount, tstamp) values (?, ?, now())");
-      // $stmt->bind_param("si", $user, $amount);
-      // $stmt->execute();
-      // $result = $stmt->get_result();
-      $command = escapeshellcmd("python3 deposit.py " . $user . " " . $amount);
-      $output = shell_exec($command);
+      $stmt = $mysqli->prepare("insert into transfers (user,amount, tstamp) values (?, ?, now())");
+      $stmt->bind_param("si", $user, $amount);
+      $stmt->execute();
+      $result = $stmt->get_result();
     }
     die('<script type="text/javascript">window.location.href="' . $url . '"; </script>');
   }
@@ -83,18 +88,44 @@ else {
   {
     if(!authenticate($user, $pass)) {
       print "Error: username and/or password is incorrect.";
-    } else {
-      // $stmt = $mysqli->prepare("insert into transfers (user,amount, tstamp) values (?, ?, now())");
-      // $stmt->bind_param("si", $user, -$amount);
-      // $stmt->execute();
-      // $result = $stmt->get_result();
-      $command = escapeshellcmd("python3 withdraw.py " . $user . " " . $amount);
-      $output = shell_exec($command);
+      print "Back to <A HREF='index.php'>home</A>";	
+    } 
+    else {
+      $stmt = $mysqli->prepare("select * from transfers where user=?");
+      $stmt->bind_param("s", $user);
+      $stmt->execute();
+      $result = $stmt->get_result();
+      $sum = 0;
+      while ($row = $result->fetch_array())
+      {
+          $tmp_amount = $row['amount'];
+          if ($tmp_amount < 0)
+          {
+            $action = "Withdrawal";
+          }
+        else
+          {
+            $action = "Deposit";
+          }
+          $sum += $tmp_amount;
+      }
+      if($sum >= $amount) {
+        $amount = -$amount;
+        $stmt = $mysqli->prepare("insert into transfers (user, amount, tstamp) values (?, ?, now())");
+        $stmt->bind_param("si", $user, $amount);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        die('<script type="text/javascript">window.location.href="' . $url . '"; </script>');
+      }
+      else {
+        print "Error: unsufficient funds.";
+        print "Back to <A HREF='index.php'>home</A>";	
+      }
     }
-    die('<script type="text/javascript">window.location.href="' . $url . '"; </script>');
   }
   else {
     print "Error: Unrecognized action";
+    print "Back to <A HREF='index.php'>home</A>";	
   }
   //Log data for scoring
   $query = "select * from transfers";
