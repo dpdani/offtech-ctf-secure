20529df00fecfe7cae8e6e272f9675b340b5a4f9
alan-gassine <gassinealan@gmail.com>
2022-12-01 05:38:06 -0500

    Improved input validation, added sign up and authentication

--- Students_Deterlab_NS/process.php
+++ Students_Deterlab_NS/process.php
@@ -11,10 +11,18 @@ $pass = clear_input($_GET["pass"]);
 $amount = (int)clear_input($_GET["amount"]);
 $choice = clear_input($_GET["drop"]);
 
-if (is_null($user) || is_null($pass) || is_null($amount) || is_null($choice) || 
-    !is_numeric($amount) || !ctype_alnum($user)) {
-  print "Error: Please check your inputs";
-} else {
+if (is_null($user) || is_null($pass) || is_null($choice)) {
+  print "Error: all the fields must be filled";
+} else if (!ctype_alnum($user)) {
+  print "Error: username can only contain alphanumeric characters.";
+} else if (strlen($user) > 20) {
+  print "Error: username must contain less than 20 characters."; // Increase the size in the db?
+} else if (strlen($pass) < 8 || strlen($pass) > 50) {
+  print "Error: password must be contain 8 to 50 characters.";
+} else if (!is_numeric($amount) || ($amount <= 0 && ($choice == "deposit" || $choice == "withdraw"))) {
+  print "Error: amount must be above 0.";
+} 
+else {
   $mysqli = new mysqli('localhost', 'root', 'rootmysql', 'ctf2');
   if (!$mysqli) 
   {
@@ -22,63 +30,63 @@ if (is_null($user) || is_null($pass) || is_null($amount) || is_null($choice) ||
   }
   $url="process.php?user=$user&pass=$pass&drop=balance";
   if ($choice == 'register')
-{
-  if(strlen($pass) < 8 || strlen($pass)>100) {
-  die('Error: the password must be 8 to 100 characters');
-  }	
-  if(strlen($user) > 100) {
-  die('Error: the username must be less that 100 characters');
-  }
-
-    $stmt = $mysqli->prepare("insert into users (user,pass) values (?, ?)");
-    $stmt->bind_param("ss", $user, $pass);
-    $stmt->execute();
-    $result = $stmt->get_result();
+  {
+    $command = escapeshellcmd("python3 sign_up.py " . $user . " " . $pass);
+    $output = shell_exec($command);
     die('<script type="text/javascript">window.location.href="' . $url . '"; </script>');
   }
   else if ($choice == 'balance')
-{
-  // todo add authentication
-    $stmt = $mysqli->prepare("select * from transfers where user=?");
-    $stmt->bind_param("s", $user);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $sum = 0;
-    print "<H1>Balance and transfer history for $user</H1><P>";
-    print "<table border=1><tr><th>Action</th><th>Amount</th></tr>";
-    while ($row = $result->fetch_array())
-    {
-        $amount = $row['amount'];
-        if ($amount < 0)
-        {
-          $action = "Withdrawal";
+  {
+    if(!authenticate($user, $pass)) {
+      print "Error: username and/or password is incorrect.";
+    } else {
+      $stmt = $mysqli->prepare("select * from transfers where user=?");
+      $stmt->bind_param("s", $user);
+      $stmt->execute();
+      $result = $stmt->get_result();
+      $sum = 0;
+      print "<H1>Balance and transfer history for $user</H1><P>";
+      print "<table border=1><tr><th>Action</th><th>Amount</th></tr>";
+      while ($row = $result->fetch_array())
+      {
+          $amount = $row['amount'];
+          if ($amount < 0)
+          {
+            $action = "Withdrawal";
+          }
+        else
+          {
+            $action = "Deposit";
+          }
+          print "<tr><td>" . $action . "</td><td>" . $amount . "</td></tr>";
+          $sum += $amount;
         }
-      else
-        {
-          $action = "Deposit";
-        }
-        print "<tr><td>" . $action . "</td><td>" . $amount . "</td></tr>";
-        $sum += $amount;
-      }
-      print "<tr><td>Total</td><td>" . $sum . "</td></tr></table>";
-      print "Back to <A HREF='index.php'>home</A>";		    
+        print "<tr><td>Total</td><td>" . $sum . "</td></tr></table>";
+        print "Back to <A HREF='index.php'>home</A>";	
+    }	    
   }
   else if ($choice == 'deposit')
-{
-  // todo add authentication
-    $stmt = $mysqli->prepare("insert into transfers (user,amount) values (?, ?, now())");
-    $stmt->bind_param("si", $user, $amount);
-    $stmt->execute();
-    $result = $stmt->get_result();
+  {
+    if(!authenticate($user, $pass)) {
+      print "Error: username and/or password is incorrect.";
+    } else {
+      $stmt = $mysqli->prepare("insert into transfers (user,amount) values (?, ?, now())");
+      $stmt->bind_param("si", $user, $amount);
+      $stmt->execute();
+      $result = $stmt->get_result();
+    }
     die('<script type="text/javascript">window.location.href="' . $url . '"; </script>');
   }
   else if ($choice == 'withdraw')
-{
-  // todo add authentication
-    $stmt = $mysqli->prepare("insert into transfers (user,amount) values (?, ?, now())");
-    $stmt->bind_param("si", $user, -$amount);
-    $stmt->execute();
-    $result = $stmt->get_result();
+  {
+    if(!authenticate($user, $pass)) {
+      print "Error: username and/or password is incorrect.";
+    } else {
+      $stmt = $mysqli->prepare("insert into transfers (user,amount) values (?, ?, now())");
+      $stmt->bind_param("si", $user, -$amount);
+      $stmt->execute();
+      $result = $stmt->get_result();
+    }
     die('<script type="text/javascript">window.location.href="' . $url . '"; </script>');
   }
   else {
@@ -91,15 +99,14 @@ if (is_null($user) || is_null($pass) || is_null($amount) || is_null($choice) ||
   fwrite($fh, "TRANSFERS\n");
   while ($row = $result->fetch_array())
   {
-      $timestamp = date('Y-m-d h:i:s', time());
-      fwrite($fh, $row['user'] . " " . $row['amount'] . " " . $row['tstamp'] . "\n");
+    fwrite($fh, $row['user'] . " " . $row['amount'] . " " . $row['tstamp'] . "\n");
   }
   $query = "select * from users";
   $result = $mysqli->query($query);
   fwrite($fh, "USERS\n");
   while ($row = $result->fetch_array())
   {
-      fwrite($fh, $row['user'] . " " . $row['pass'] . "\n");
+    fwrite($fh, $row['user'] . " " . $row['pass'] . "\n");
   }
   fwrite($fh, "END\n");
 }
@@ -109,6 +116,12 @@ function clear_input($data) {
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
+}
+
+function authenticate($user, $pass) {
+  $command = escapeshellcmd("python3 login.py " . $user . " " . $pass);
+  $output = shell_exec($command);
+  return $output;
 }
 ?>
 
